ab3ddcc3c3a129d07c56daf8b7e35425b4acfabe
BrettCarter <brettcartersud@gmail.com>
2015-09-21 08:06:10 -0600

    Changes to Board and adding Column class

---
+++ CS3750CardTrick/CS3750CardTrick/Column.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS3750CardTrick
+{
+    class Column
+    {
+        int id;
+
+        public static void addCard(Card card)
+        {
+
+        }
+    }
+}
