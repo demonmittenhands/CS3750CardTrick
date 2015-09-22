b38fd282c2f1e9e641e6f159559d3b59a78f4ab9
demonmittenhands <demonmittenhands@gmail.com>
2015-09-22 14:56:26 -0600

    Cards are displaying.

--- CS3750CardTrick/CS3750CardTrick/Dealer.cs
+++ CS3750CardTrick/CS3750CardTrick/Dealer.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Controls;
+using Microsoft.CSharp;
+using System.CodeDom.Compiler;
 
 namespace CS3750CardTrick
 {
@@ -37,7 +39,7 @@ namespace CS3750CardTrick
             MainWindow window = App.Current.Windows.OfType<MainWindow>().SingleOrDefault(x => x.IsActive);
 
             ImageSourceConverter imgs = new ImageSourceConverter();
-            window.imgTest1.SetValue(Image.SourceProperty, imgs.ConvertFromString(@"Deck/2_of_clubs.png"));
+            //window.imgTest1.SetValue(Image.SourceProperty, imgs.ConvertFromString(@"Deck/2_of_clubs.png"));
             
             window.lblTest.Content = "balogna";
 
@@ -47,10 +49,10 @@ namespace CS3750CardTrick
 
             Deal();
 
-           ShowCards(board);
+           ShowCards(deck);
         }
 
-        public void ShowCards(Board board)
+        public void ShowCards(Deck deck)
         {
             string imgSource;
             List<Card> cardList = new List<Card>();
@@ -61,26 +63,54 @@ namespace CS3750CardTrick
 
             //Image img = new Image();
 
-            for (int i = 0; i < 3; i++)
-            {
-                columnTemp = board.getColumn(i);
-                for (int j = 0; j < 7; j++)
-                {
+            
 
-                    RectangleGeometry rect = new RectangleGeometry(new System.Windows.Rect(new System.Windows.Size(363, 250)));
-                    Image img = new Image();
-                    //img.RenderSize = ;
+           // for (int i = 0; i < 12; i++)
+           // {
 
-                    img.Height = 120;
-                    img.Width = 75;
-                    img.Stretch = Stretch.Fill;
-                    cardListTemp = columnTemp.getCardList();
-                    imgSource = "" + cardListTemp[j].getFace();
-                    img.Source = imgs.ConvertFromString(@imgSource) as ImageSource;
-                    Grid.SetColumn(img, i);
-                    window.grdCardGrid.Children.Add(img);
-                }
-            }
+                RectangleGeometry rect = new RectangleGeometry(new System.Windows.Rect(new System.Windows.Size(363, 250)));
+                Image img = new Image();
+                //img.RenderSize = ;
+
+                img.Height = 60;
+                img.Width = 25;
+                img.Stretch = Stretch.Fill;
+                //img.Source = imgs.ConvertFromString("" + deck21[0].getFace()) as ImageSource;
+
+                window.img1.Source = imgs.ConvertFromString("" +  deck21[0].getFace()) as ImageSource;
+                window.img2.Source = imgs.ConvertFromString("" +  deck21[1].getFace()) as ImageSource;
+                window.img3.Source = imgs.ConvertFromString("" +  deck21[2].getFace()) as ImageSource;
+                window.img4.Source = imgs.ConvertFromString("" +  deck21[3].getFace()) as ImageSource;
+                window.img5.Source = imgs.ConvertFromString("" +  deck21[4].getFace()) as ImageSource;
+                window.img6.Source = imgs.ConvertFromString("" +  deck21[5].getFace()) as ImageSource;
+                window.img7.Source = imgs.ConvertFromString("" +  deck21[6].getFace()) as ImageSource;
+                window.img8.Source = imgs.ConvertFromString("" +  deck21[7].getFace()) as ImageSource;
+                window.img9.Source = imgs.ConvertFromString("" +  deck21[8].getFace()) as ImageSource;
+                window.img10.Source = imgs.ConvertFromString("" + deck21[9].getFace()) as ImageSource;
+                window.img11.Source = imgs.ConvertFromString("" + deck21[10].getFace()) as ImageSource;
+                window.img12.Source = imgs.ConvertFromString("" + deck21[11].getFace()) as ImageSource;
+                window.img13.Source = imgs.ConvertFromString("" + deck21[12].getFace()) as ImageSource;
+                window.img14.Source = imgs.ConvertFromString("" + deck21[13].getFace()) as ImageSource;
+                window.img15.Source = imgs.ConvertFromString("" + deck21[14].getFace()) as ImageSource;
+                window.img16.Source = imgs.ConvertFromString("" + deck21[15].getFace()) as ImageSource;
+                window.img17.Source = imgs.ConvertFromString("" + deck21[16].getFace()) as ImageSource;
+                window.img18.Source = imgs.ConvertFromString("" + deck21[17].getFace()) as ImageSource;
+                window.img19.Source = imgs.ConvertFromString("" + deck21[18].getFace()) as ImageSource;
+                window.img20.Source = imgs.ConvertFromString("" + deck21[19].getFace()) as ImageSource;
+                window.img21.Source = imgs.ConvertFromString("" + deck21[20].getFace()) as ImageSource;
+                                                                         
+
+
+
+                //string imgname;
+
+
+                //string imgname = ("img"+i);
+                
+                
+               // window.imgname.Source = imgSource;
+                
+           // }
 
 
         }
